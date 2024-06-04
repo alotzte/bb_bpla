612b5b40bc504e218bec5361327a3883016bed91
ifonya22 <senior.afonichev@gmail.com>
2024-06-04 13:18:16 +0300

    Добавил ручку /predict_video

--- ml/app/main.py
+++ ml/app/main.py
@@ -1,16 +1,22 @@
+import os
 from typing import List
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.concurrency import run_in_threadpool
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+# import aiofiles
+from tempfile import NamedTemporaryFile
+# import asyncio
 from PIL import Image
 import io
+import cv2
 
 from ml_model.yolo import YoloModel
 from ml_model.exceptions.exp import ZeroObjectsDetected
 
 
 class Message(BaseModel):
-    message: str
+    message: str = 'Не найдено ни одного объекта'
 
 
 app = FastAPI()
@@ -22,14 +28,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-model = YoloModel("ml_model/weights/best.pt")
+model = YoloModel("ml_model/weights/yolov10n.pt")
 
 
 @app.post(
-        "/predict_photo",
+        "/ml/predict_photo",
         description="Кидаешь массив с изображениями и получаешь список путей фото с bbox и txt с разметкой",
         responses={
-            404: {"model": Message, "description": "Zero objects detected"},
+            404: {
+                "model": Message,
+                "description": "Zero objects detected"
+            },
             200: {
                 "description": "Successful Response",
                 "content": {
@@ -37,8 +46,8 @@ model = YoloModel("ml_model/weights/best.pt")
                         "example": {
                             "data": [
                                 {
-                                    "path_to_photo_with_bbox": "Путь к фото c bbox :str",
-                                    "path_to_txt": "Путь к txt файлу :str"
+                                    "upd_photo_path": "Путь к фото c bbox",
+                                    "txt_path": "Путь к txt файлу"
                                 }
                             ]
                         }
@@ -48,11 +57,10 @@ model = YoloModel("ml_model/weights/best.pt")
         },
 )
 async def predict_photo(
-    files: List[UploadFile] = File(...)
-
+    files: List[UploadFile] = File(description='Загрузка фото')
 ):
     photos_data = []
-
+    # TODO: добавить ограничение на расширения
     try:
         for file in files:
             image = Image.open(io.BytesIO(await file.read()))
@@ -60,8 +68,8 @@ async def predict_photo(
             data = model.predict_photos(image, file.filename)
             photos_data.append(
                 {
-                    "path_to_photo_with_bbox": data['path_to_photo_with_bbox'],
-                    "path_to_txt": data['path_to_txt']
+                    "upd_photo_path": data['upd_photo_path'],
+                    "txt_path": data['txt_path']
                 }
             )
         if len(photos_data) == 0:
@@ -77,8 +85,76 @@ async def predict_photo(
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@app.post('/predict_video')
-async def predict_video(
-    file: UploadFile = File(...)
-):
-    pass  # TODO: доделать предикт видео
+# @app.post(
+#     '/ml/predict_video_async',
+# )
+# async def predict_video(
+#     file: UploadFile = File(...)
+# ):
+#     try:
+#         async with aiofiles.tempfile.NamedTemporaryFile("wb", delete=False) as temp:
+#             try:
+#                 contents = await file.read()
+#                 await temp.write(contents)
+#             except Exception:
+#                 return {"message": "There was an error uploading the file"}
+#             finally:
+#                 await file.close()
+
+#         res = await run_in_threadpool(process_video, temp.name)  # Pass temp.name to VideoCapture()
+#     except Exception:
+#         return {"message": "There was an error processing the file"}
+#     finally:
+#         os.remove(temp.name)
+
+#     return res
+
+
+@app.post(
+    "/ml/predict_video",
+    description="Кидаешь видео, получаешь путь к видео и список интервалов",
+    responses={
+        200: {
+            "description": "Successful Response",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "upd_video_path": "Путь к видео c bbox",
+                        "intervals": "Интервалы (начало_эпизода:конец_эпизода)"
+                    }
+                }
+            },
+        },
+        404: {
+            "model": Message,
+            "description": "Zero objects detected"
+        },
+    })
+def detect_faces(file: UploadFile = File(...)):
+    suffix = os.path.splitext(file.filename)[1]
+    temp = NamedTemporaryFile(delete=False, suffix=suffix)
+    try:
+        try:
+            contents = file.file.read()
+            with temp as f:
+                f.write(contents)
+        except Exception:
+            return {"message": "There was an error uploading the file"}
+        finally:
+            file.file.close()
+        # print(temp.name)
+        intervals = model.predict_video(temp.name)
+        if len(intervals) == 0:
+            raise ZeroObjectsDetected
+
+    except ZeroObjectsDetected as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        return {"message": f"There was an error processing the file:\n{str(e)}"}
+    finally:
+        os.remove(temp.name)
+
+    return {
+        'upd_video_path': file.filename,
+        'intervals': intervals
+    }
--- ml/app/ml_model/yolo.py
+++ ml/app/ml_model/yolo.py
@@ -1,13 +1,16 @@
+from datetime import datetime, timedelta
 import os
 from ultralytics import YOLOv10
+import cv2
+
 
 FULL_PATH_TO_TXT = '/app/ml_model/results/'
+FULL_PATH_TO_VIDEO = '/app/ml_model/results/'
 
 
 class YoloModel:
     def __init__(self, model_path):
-        self.model = YOLOv10('/app/ml_model/weights/yolov10n.pt')
-        # self.model = YOLOv10(model_path)
+        self.model = YOLOv10(model_path)
 
     def predict_photos(self, img, filename):
         width, height = img.size
@@ -18,50 +21,91 @@ class YoloModel:
         )  # TODO augment
 
         # TODO: создать папки
-        name_img = f"""{
+        upd_photo_path = f"""{
             os.path.join(
                 FULL_PATH_TO_TXT, 'img', os.path.splitext(filename)[0])}.jpg"""
 
-        name_txt = f"""{
+        txt_path = f"""{
             os.path.join(
                 FULL_PATH_TO_TXT, 'txt', os.path.splitext(filename)[0])}.txt"""
 
         # TODO: Продумать куда сохранять
-        res[0].save(name_img)
-        res[0].save_txt(name_txt)
+        res[0].save(upd_photo_path)
+        res[0].save_txt(txt_path)
         # TODO: Проверить сохранение
 
         return {
-            'path_to_photo_with_bbox': name_img,
-            'path_to_txt': name_txt
+            'upd_photo_path': upd_photo_path,
+            'txt_path': txt_path
         }
 
-        # bbox_list = []
-        # try:
-        #     for r in res[0].boxes:
-        #         box_cls, box_xywh = int(r.cls.to("cpu").tolist()[0]), \
-        #             r.xywh.to("cpu").tolist()[0]
-        #         bbox_list.append(self._generate_txt_string(box_cls, box_xywh))
+    def predict_video(self, video_path):
+        last_frame_success = False
+        fps = self.get_video_info(video_path)
+        intervals = {}
 
-        #     if len(bbox_list) > 0:
-        #         return self._generate_txt_file_from_list(
-        #             bbox_list,
-        #             os.path.join(
-        #                 FULL_PATH_TO_TXT,
-        #                 os.path.splitext(filename)[0] + '.txt'
-        #             )
-        #         )
-        #     else:
-        #         return None
-        # except:
-        #     return None
+        for frame_number, r in enumerate(self.model.predict(
+            video_path,
+            conf=0.5,
+            save=True,
+            stream=True,
+            # imgsz=(height, width),
+        )):
+            print(len(r.boxes.xywh))
+            if len(r.boxes.xywh) > 0:
+                if not last_frame_success:
+                    start_timing = self.get_timing(fps, frame_number)
+                    intervals[start_timing] = ''
+                end_timing = self.get_timing(fps, frame_number)
+                last_frame_success = True
+            else:
+                if last_frame_success:
+                    intervals[start_timing] = end_timing
+                last_frame_success = False
 
-    def _generate_txt_string(self, box_cls, box_xywh):
-        return f'{box_cls} {" ".join(map(str, box_xywh))}'
+        if last_frame_success:
+            intervals[start_timing] = end_timing
 
-    def _generate_txt_file_from_list(self, bbox_list, full_path):
-        with open(full_path, 'w') as f:
-            for bbox in bbox_list:
-                f.write(bbox + '\n')
+        return self.merge_intervals(intervals)
 
-        return full_path
+    def get_timing(self, fps, frame_number):
+        frame_time = frame_number / fps
+        hours = int(frame_time // 3600)
+        minutes = int((frame_time % 3600) // 60)
+        seconds = int(frame_time % 60)
+        formatted_time = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+        return formatted_time
+
+    def get_video_info(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        return fps
+
+    def merge_intervals(self, data):
+        def str_to_time(time_str):
+            return datetime.strptime(time_str, "%H:%M:%S")
+
+        def time_to_str(time_obj):
+            return time_obj.strftime("%H:%M:%S")
+
+        intervals = [
+            (str_to_time(start), str_to_time(end))
+            for start, end in data.items()]
+        intervals.sort()
+
+        merged_intervals = []
+        for start, end in intervals:
+            if not merged_intervals:
+                merged_intervals.append((start, end))
+            else:
+                last_start, last_end = merged_intervals[-1]
+                if last_end < start - timedelta(seconds=1):
+                    merged_intervals.append((start, end))
+                else:
+                    merged_intervals[-1] = (last_start, max(last_end, end))
+
+        result = {
+            time_to_str(start):
+            time_to_str(end) for start, end in merged_intervals
+        }
+        return result
